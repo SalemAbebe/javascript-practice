de4dd1034138095f5861d19310eafcf022f551ff
Salem Abebe <salemabebe@yahoo.com>
2021-09-23 16:54:39 -0400

    day-2

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ console.log("Eyerusalem Abebe");
 
 // my favorite food
 console.log("Doro wot")
+
+
+// daily challenge-2
+
+console.log("Supercalifragilisticexpialidocious".length);
+
+console.log(12345);
+console.log(typeof (12345));
+
+// BigInt
+console.log(123546587848n);
+console.log(typeof (123546587848n));
+
